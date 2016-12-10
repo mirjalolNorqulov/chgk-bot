a0987ea66ac04139c4e4f7cbd668bb48442de7d3
mirjalolNorqulov <mirjalol2401@gmail.com>
2016-12-10 22:45:25 +0500

    Create wsgi.py

---
+++ wsgi.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+
+# Copyright (C) 2016 Mirjalol Norqulov
+#
+
+# 
+import os
+import bot
+import telebot
+from flask import Flask, request
+from telebot import types
+
+
+application = Flask(__name__, instance_path=os.environ['OPENSHIFT_REPO_DIR'])
+update_queue, bot_instance = bot.setup(webhook_url='https://{}/{}'.format(
+    os.environ['OPENSHIFT_GEAR_DNS'],
+    bot.TOKEN
+))
+
+
+@application.route('/')
+def not_found():
+    """Server won't respond in OpenShift if we don't handle the root path."""
+    return ''
+
+
+@application.route('/' + bot.TOKEN, methods=['GET', 'POST'])
+def webhook():
+    if request.json:
+        #update_queue.put(Update.de_json(request.json, bot_instance))
+        update = telebot.types.Update.de_json(request.json)
+        bot.bot.process_new_messages([update.message])
+    return ''
+
+
+if __name__ == '__main__':
+    ip = os.environ['OPENSHIFT_PYTHON_IP']
+    port = int(os.environ['OPENSHIFT_PYTHON_PORT'])
+    application.run(host=ip, port=port)
