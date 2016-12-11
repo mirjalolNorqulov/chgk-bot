b136803f384aa1c713aa5f2962a2454831f03440
mirjalolNorqulov <mirjalol2401@gmail.com>
2016-12-11 11:51:19 +0500

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -29,7 +29,7 @@ def getMessage():
 def webhook():
     bot.remove_webhook()
     bot.set_webhook(url="https://py-awesome.herokuapp.com/bot")
-    return "!", 200
+    return "Hello heroku"
 
 server.run(host="0.0.0.0", port=os.environ.get('PORT', 5000))
 server = Flask(__name__)
