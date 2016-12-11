951e3634776f7bff0caa17822208830b56edbd97
mirjalolNorqulov <mirjalol2401@gmail.com>
2016-12-11 13:04:51 +0500

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -15,14 +15,6 @@ from flask import Flask, request
 
 server = Flask(__name__)
 
-@bot.message_handler(commands=['start'])
-def start(message):
-    bot.reply_to(message, 'Hello, ' + message.from_user.first_name)
-
-@bot.message_handler(func=lambda message: True, content_types=['text'])
-def echo_message(message):
-    bot.reply_to(message, message.text)
-
 @server.route("/bot", methods=['POST'])
 def getMessage():
     bot.process_new_updates([telebot.types.Update.de_json(request.stream.read().decode("utf-8"))])
