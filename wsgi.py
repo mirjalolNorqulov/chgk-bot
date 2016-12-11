20df5951a39dc59c02b4a6bc9ce0c72d4cc79d00
mirjalolNorqulov <mirjalol2401@gmail.com>
2016-12-11 11:34:55 +0500

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -4,9 +4,33 @@
 #
 
 # 
+import telebot
+import os
+from flask import Flask, request
 
-import bot
+bot = telebot.TeleBot('237816678:AAEg8NJtmsUCUuyg7nFokTYVt1w5vN4EW8M')
 
-if __name__ == '__main__':
-    bot.bot.polling(none_stop = True)
-    
+server = Flask(__name__)
+
+@bot.message_handler(commands=['start'])
+def start(message):
+    bot.reply_to(message, 'Hello, ' + message.from_user.first_name)
+
+@bot.message_handler(func=lambda message: True, content_types=['text'])
+def echo_message(message):
+    bot.reply_to(message, message.text)
+
+@server.route("/bot", methods=['POST'])
+def getMessage():
+    bot.process_new_updates([telebot.types.Update.de_json(request.stream.read().decode("utf-8"))])
+    return "!", 200
+
+@server.route("/")
+def webhook():
+    bot.remove_webhook()
+    bot.set_webhook(url="https://py-awesome.herokuapp.com/bot")
+    return "!", 200
+
+server.run(host="0.0.0.0", port=os.environ.get('PORT', 5000))
+server = Flask(__name__)
+   
